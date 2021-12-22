afd1c36d4c2396957442533eef3a862e39b906a1
Rikkkkk <95572800+RickR063r7@users.noreply.github.com>
2021-12-22 19:27:13 -0300

    Update bs2bs.js

--- bs2bs.js
+++ bs2bs.js
@@ -6,10 +6,32 @@ function bs2bs(v,b1,b2) {
  let b10=0
  let v2= []
  let v22=[]
- const NB = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+ const NB = {
+  '0': 0,
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  '10': 'A',
+  '11': 'B',
+  '12': 'C',
+  '13': 'D',
+  '14': 'E',
+  '15': 'F',
+  'a': 10,
+  'b': 11,
+  'c': 12,
+  'd': 13,
+  'e': 14,
+  'f': 15}
  
  for (let i = 0; i < v.length; i++) {
-  b10+=(parseInt(v[v.length-1-i]))*(b1**i)
+  b10+=(NB[(v[v.length-1-i]).toLowerCase()])*(b1**i)
  }
  if(b2!=10){
   while(b10!=0){
@@ -18,8 +40,8 @@ function bs2bs(v,b1,b2) {
   
   for (let i = 0; i < v2.length; i++) {
    v22+=v2[v2.length-1-i]}
-   console.log(v22)} else {
-   console.log(b10)
+  console.log(v22)} else {
+  console.log(b10)
  }
 }
 
